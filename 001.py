35b21df9af939b3899d3fcdfe415ad2d90fd21e5
ryusuke920 <ryusuke920@icloud.com>
2022-02-03 18:26:52 +0900

    修正を加えました

--- 001.py
+++ 001.py
@@ -2,8 +2,7 @@ N, L = map(int, input().split())
 K = int(input())
 A_L = list(map(int, input().split()))
 
-
-def make_piece_l(L,length):
+def make_piece_l(L: list, length: int) -> list:
     return_l = []
     tmp_length = 0
 
@@ -11,10 +10,10 @@ def make_piece_l(L,length):
         return_l.append(a - tmp_length)
         tmp_length = a
     return_l.append(length - L[-1])
+
     return return_l
 
-
-def calc_score(n):
+def calc_score(n: int) -> bool:
     return_l = []
     score = 0
     for a in A_L:
@@ -23,22 +22,17 @@ def calc_score(n):
             return_l.append(score)
             score = 0
 
-    if len(return_l) > K:
-        return True
-    else:
-        return False
+    return len(return_l) > K
 
+A_L = make_piece_l(A_L, L)
+l, r = 0, L
 
-A_L = make_piece_l(A_L,L)
-l = 0
-r = L
+while r - l > 1:
+    mid = (l + r) // 2
 
-while True:
-    mid = (l + r)//2
-    if r == mid or l == mid:
-        print(mid)
-        break
     if calc_score(mid):
         l = mid
     else:
         r = mid
+
+print(l)
